6d0fca5df4d4ed11030ecc4bf2ee2cbf3f5a8ac3
arkadip302 <1706302@kiit.ac.in>
2020-06-28 18:14:27 +0530

    commit

---
+++ Chotomamu/transformword.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string source, target;
+    getline(cin, source);
+    getline(cin, target);
+    if (source.length() != target.length())
+    {
+    }
+}
